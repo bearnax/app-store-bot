7dc0841d8addfbfe36e9ede3fa979e672017c8e0
Will Brodnax <wbrodnax@gmail.com>
2018-09-13 20:27:22 -0400

    add postgres functions

--- app.py
+++ app.py
@@ -6,22 +6,89 @@ import psycopg2
 
 
 
-
-
-# verification_token = os.environ['VERIFICATION_TOKEN']
+verification_token = os.environ['VERIFICATION_TOKEN']
 app = Flask(__name__)
-
 app_ids = (711074743, 418075935, 1098201243)
-
+# TODO: move app ids to database
 base_apple_url = "https://itunes.apple.com/lookup?id="
+
+
+def connect():
+    """ Connect to PostgreSQL server """
+
+    try:
+        #retrieve params from environment
+        env_host = os.environ['PG_HOST']
+        env_database = os.environ['PG_DATABASE']
+        env_user = os.environ['PG_USER']
+        env_password = os.environ['PG_PASSWORD']
+
+        return psycopg2.connect(
+            host = env_host,
+            dbname = env_database,
+            user = env_user,
+            password = env_password
+        )
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+
+def get_sql_data(query, *args):
+    """ retrieve data from the postgresql database """
+
+    try:
+        assert query.count('%s') == len(args)
+
+        conn = connect()
+        cursor = conn.cursor()
+
+        cursor.execute(query, args)
+
+        data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        # print("SUCCESS: get_sql_data")
+
+        return data
+
+    except AssertionError:
+        print("ERROR: get_sql_data(), wrong number of arguments")
+
+
+
+def post_sql_data(query, *args):
+    """ insert data into the postgresql database
+        This function can be used to post new data or delete data
+
+        Post / Delete should be indicated in the db query
+
+    """
+
+    try:
+        assert query.count('%s') == len(args)
+
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        # print("SUCCESS: post_sql_data")
+
+    except AssertionError:
+        print("ERROR: post_sql_data(), wrong number of arguments")
 
 
 
 def request_data_from_apple(url, args):
-    """ retrieve app data from apple
+    """ Retrieve app data from the Apple App Atore via
+    the Apple App Store url (api) call
 
     Params:
-        base_apple_url:type(string)
+        url:type(string) > must be apple store api url + '?id=' arg
         args:type(int) > must be valid 10 digit app ids
     """
 
@@ -50,6 +117,23 @@ def app_bot():
             # TODO: write some code here please
             pass
 
+        elif command == 'add_new app':
+            """ initiate data ingestion manually
+            """
+            # TODO: write some code here please, this one probaby needs 'banter'
+            pass
+
+        elif command == 'hide app from ranking':
+            """ initiate data ingestion manually
+            """
+            # TODO: write some code here please, this one probaby needs 'banter'
+            pass
+
+        elif command == 'delete app':
+            """ initiate data ingestion manually
+            """
+            # TODO: write some code here please, this one probaby needs 'banter'
+            pass
 
         elif command == 'rank today':
             """ initiate data ingestion manually
